2f0115a152ec62dea99d7f2b117b26da569b24a9
Bruno Oliveira <bruno@abstractj.org>
2013-02-27 13:31:21 -0300

    Switch to stable dependencies from AG-Sec and AG-Controller

--- server/src/main/java/org/aerogear/todo/server/util/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package org.aerogear.todo.server.util;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/auth")
-public class JaxRsActivator extends Application {
-    /* class body intentionally left blank */
-}
